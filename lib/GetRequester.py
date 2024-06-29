e6c7ab4749c82e1215e77b31d5447d48ba17f196
Favor Vincent <vfavorm77@gmail.com>
2024-06-29 10:29:59 +0300

    remote-data lab complete

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,19 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
+        response = requests.get(self.url)
+        return response.content
+    
         pass
 
     def load_json(self):
+        result = []
+        responses = json.loads(self.get_response_body())
+        
+        for response in responses:
+            result.append(response)
+        return result
         pass
+
+get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+get_requester.load_json()
